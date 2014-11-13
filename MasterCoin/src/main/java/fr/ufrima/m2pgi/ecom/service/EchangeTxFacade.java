0aefe38b1bd3f5f6999b76437e63c4a59144161e
chambora <chambon.raphael@gmail.com>
2014-11-13 14:14:51 +0100

    Ajout d'un bouton/Changement des boutons échanges et offre/

---
+++ MasterCoin/src/main/java/fr/ufrima/m2pgi/ecom/service/EchangeTxFacade.java
@@ -0,0 +1,13 @@
+package fr.ufrima.m2pgi.ecom.service;
+
+import fr.ufrima.m2pgi.ecom.model.Transaction;
+
+public class EchangeTxFacade {
+
+	public void trouverOffres(Transaction newTransaction) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+}
---
+++ MasterCoin/src/main/java/fr/ufrima/m2pgi/ecom/service/TransactionFacade.java
@@ -0,0 +1,37 @@
+package fr.ufrima.m2pgi.ecom.service;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import fr.ufrima.m2pgi.ecom.model.Transaction;
+
+@Stateless
+public class TransactionFacade
+{
+    @Inject
+    private EntityManager em;
+    
+    public void create(Transaction transaction) {
+        em.persist(transaction);
+    }
+
+    public void edit(Transaction transaction) {
+        em.merge(transaction);
+    }
+
+    public void remove(Transaction transaction) {
+        em.remove(em.merge(transaction));
+    }
+    
+    public Transaction find(Long id) {
+        return em.find(Transaction.class, id);
+    }
+
+    @SuppressWarnings("unchecked")
+	public List<Transaction> findAll() {
+        return em.createQuery("select object(t) from Transaction as t").getResultList();
+    }
+}
