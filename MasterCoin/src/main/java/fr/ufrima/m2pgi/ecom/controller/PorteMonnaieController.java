8cc2523a9c393dbd6367d3df6ec30db66d55e779
courbije <jeremie.courbis@gmail.com>
2014-11-20 11:57:54 +0100

    Permet de retirer de l'argent sur son compte

--- MasterCoin/src/main/java/fr/ufrima/m2pgi/ecom/controller/PorteMonnaieController.java
+++ MasterCoin/src/main/java/fr/ufrima/m2pgi/ecom/controller/PorteMonnaieController.java
@@ -1,20 +1,24 @@
 package fr.ufrima.m2pgi.ecom.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+import javax.validation.constraints.Min;
 
 import fr.ufrima.m2pgi.ecom.facade.PorteMonnaieFacade;
 import fr.ufrima.m2pgi.ecom.model.Monnaie;
 import fr.ufrima.m2pgi.ecom.model.PorteMonnaie;
+import fr.ufrima.m2pgi.ecom.service.NotEnoughtMoneyException;
 import fr.ufrima.m2pgi.ecom.service.PorteMonnaieService;
 
-@ViewScoped
+@RequestScoped
 @ManagedBean
 public class PorteMonnaieController {
 
@@ -32,13 +36,14 @@ public class PorteMonnaieController {
 
 	private Monnaie monnaie;
 
+	@Min(0)
 	private Integer amount;
 
 	public void setLogin(Login login) {
 		this.login = login;
 	}
 
-	public void register() throws Exception {
+	public void registerAdd() throws Exception {
 		try {
 			porteMonnaieService.addToPorteMonnaie(login.getCurrentUser(), monnaie, amount);
 			FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO, "Registered!", "Registration successful");
@@ -48,10 +53,44 @@ public class PorteMonnaieController {
 			FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage, "Registration unsuccessful");
 			facesContext.addMessage(null, m);
 		}
+		init();
+	}
+	
+
+	private void init() {
+		amount = null;
+		monnaie = null;
+	}
+
+	public void registerRemove() throws Exception {
+		try {
+			porteMonnaieService.removeToPorteMonnaie(login.getCurrentUser(), monnaie, amount);
+			FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO, "Registered!", "Registration successful");
+			facesContext.addMessage(null, m);
+		} catch (NotEnoughtMoneyException e) {
+			FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Pas assez d'argents", "Registration unsuccessful");
+			facesContext.addMessage(null, m);
+		}catch (Exception e) {
+			String errorMessage = getRootErrorMessage(e);
+			FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage, "Registration unsuccessful");
+			facesContext.addMessage(null, m);
+		}
+		init();
 	}
 
 	public List<PorteMonnaie> getPorteMonnaies() {
 		return porteMonnaieFacade.findByCompte(login.getCurrentUser());
+	}
+	
+	public List<Monnaie> getNotEmptyMonnaies() {
+		List<PorteMonnaie> pm = porteMonnaieFacade.findByCompte(login.getCurrentUser());
+		List<Monnaie> res = new ArrayList<Monnaie>();
+		for (PorteMonnaie p : pm) {
+			if (p.getMontant() > 0) {
+				res.add(p.getMonnaie());				
+			}
+		}
+		return res;
 	}
 
 	private String getRootErrorMessage(Exception e) {
